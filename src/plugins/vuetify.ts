99b8114ddf9b388042106869e2216f86cdd14e72
misakibwbnw <mmys.a.fk@gmail.com>
2021-01-31 01:20:55 +0900

    インデント、テーマカラーの設定

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,17 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.grey.darken1,
+        secondary: colors.grey.lighten4,
+        accent: colors.grey.base
+      }
+    }
+  }
+});
